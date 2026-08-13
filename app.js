8b1b5bbfa4d994795f949373792ce458960ea12d
RekaRajendran <reka1901@gmail.com>
2026-08-13 06:31:18 +0530

    ignitingourapp

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ import ReactDOM from 'react-dom/client';
 const parent=React.createElement(
     "div",
     {id:"parent"},
-    [    React.createElement("div",{id:"child1"},[
+    [    React.createElement("div",{id:"child1",key:"child1"},[
         React.createElement("h1",{id:"heading"},"I am an h1 tag"),
         React.createElement("h2",{id:"heading2"},"I am an h2 tag" )
     ]
     ),
-        React.createElement("div",{id:"child2"},[
+        React.createElement("div",{id:"child2",key:"child2"},[
         React.createElement("h1",{id:"heading"},"I am an h1 tag"),
         React.createElement("h2",{id:"heading3"},"I am an h2 tag" )
     ]
