54d5228ae21c4b409f6e744dfc7d731f6737c526
RekaRajendran <reka1901@gmail.com>
2026-08-13 08:45:47 +0530

    ignitingourapp-addkey

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const parent=React.createElement(
     "div",
     {id:"parent"},
     [    React.createElement("div",{id:"child1",key:"child1"},[
-        React.createElement("h1",{id:"heading"},"I am an h1 tag"),
-        React.createElement("h2",{id:"heading2"},"I am an h2 tag" )
+        React.createElement("h1",{id:"heading1",key:"heading1"},"I am an h1 tag"),
+        React.createElement("h2",{id:"heading2",key:"heading2"},"I am an h2 tag" )
     ]
     ),
         React.createElement("div",{id:"child2",key:"child2"},[
-        React.createElement("h1",{id:"heading"},"I am an h1 tag"),
-        React.createElement("h2",{id:"heading3"},"I am an h2 tag" )
+        React.createElement("h1",{id:"heading3",key:"heading3"},"I am an h1 tag"),
+        React.createElement("h2",{id:"heading4",key:"heading4"},"I am an h2 tag" )
     ]
     )
 ]
