f65047bbb20f2ce72c487cb16679e8564c9863a6
RekaRajendran <reka1901@gmail.com>
2026-08-20 10:04:16 +0530

    04-Restaurant list view

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
     import React from 'react';
     import ReactDOM from 'react-dom/client';
+    import resList from "./data.json";
 
 /***
  * Header
@@ -40,15 +41,25 @@ const Header=()=>{
 
 const RestaurantCard=(props)=>{  
     console.log(props);
-    const{resName,cusines}=props;
+    const{resData}=props;
+    const{cloudinaryImageId,name, avgRating,cuisines,costForTwo, deliveryTime }=resData?.data || {};
     return(
         <div className="res-card" style={{ backgroundColor: "#f0f0f0", borderRadius: "5px" }}>
             <img className='res-logo' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/29/6872a1fc-f7e6-4433-b8c7-6f085567689a_900503.JPG" alt="Restaurant" />
-            <h3>{resName}</h3>
-            <h4>Cuisines: {cusines}</h4>
-            <h4>Rating: 4.5</h4>
-            <h4>Delivery Time: 30-45 mins</h4>
-            <h4>Cost for Two: $20</h4>            
+             {/* <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          resData.data.cloudinaryImageId
+        }
+      /> */}
+            <h3>{name}</h3>
+            <h4>Cuisines: {cuisines.join(", ")}</h4>
+            <h4>Rating: {avgRating}</h4>
+            <h4>Cost for Two: ${costForTwo/100} </h4>  
+            <h4>Delivery Time: {resData.data.deliveryTime} mins</h4>
+                      
             
         </div>
     )
@@ -59,11 +70,12 @@ const Body=()=>{
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-               <RestaurantCard resName="Pizza Place" cusines="Italian, Chinese" rating={4.5} deliveryTime="30-45 mins" costForTwo="$20" />
-               <RestaurantCard resName="Burger King" cusines="American, Fast Food" rating={4.0} deliveryTime="20-30 mins" costForTwo="$15" />
-               <RestaurantCard resName="Sushi Master" cusines="Japanese, Sushi" rating={4.8} deliveryTime="40-50 mins" costForTwo="$25" />
-               <RestaurantCard resName="Taco Bell" cusines="Mexican, Fast Food" rating={4.2} deliveryTime="15-25 mins" costForTwo="$12" />
-                 
+                  {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+              
+             
+              
             </div>
         </div>
     )
